7e29d084c97cb078bae411d895513a795239a763
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2023-07-23 02:48:23 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,3 +1,3 @@
-local BaseURL = 'https://raw.githubusercontent.com/MaGiXxScripter0/montehub/main/'
+local BaseURL = 'https://raw.githubusercontent.com/MaGiXxScripter0/montehub/main/games/'
 
 loadstring(game:HttpGet(BaseURL..tostring(game.PlaceId)))()
